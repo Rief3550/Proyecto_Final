212eeaed09e84e59776e3c48846079056021750c
RieraFederico <113047761+Rief3550@users.noreply.github.com>
2022-10-18 13:06:35 -0300

    agreagr ejemplos de clases 18

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "ejemplo/saludar.html")
+    return render(request, "ejemplo/saludar.html",{"nombre":"German",
+    "apellido": "Martinez"})
+
+def index_dos(request,nombre, apellido):
+    return render(request,"ejemplo/saludar.html",
+    {
+        "nombre":nombre, 
+        "apellido": apellido,
+    })
+def index_tres(request):
+    return render(request,"ejemplo/saludar.html", 
+{"notas":[1,2,3,4,5,6,7,8]}
+    )
+def imc(request,peso,altura):
+    return render(request,"ejemplo/imc.html",{"imc":imc})
