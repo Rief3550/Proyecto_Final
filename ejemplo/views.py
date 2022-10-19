905c8cb38411e11116f565493102cb295fcf1671
RieraFederico <113047761+Rief3550@users.noreply.github.com>
2022-10-18 23:21:28 -0300

    primer commit de mi proyecto

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from ejemplo.models import Familiar
 
 def index(request):
     return render(request, "ejemplo/saludar.html",{"nombre":"German",
@@ -16,3 +17,8 @@ def index_tres(request):
     )
 def imc(request,peso,altura):
     return render(request,"ejemplo/imc.html",{"imc":imc})
+
+def monstrar_familiares(request):
+  lista_familiares = Familiar.objects.all()
+  return render(request, "ejemplo/familiares.html", 
+               {"lista_familiares": lista_familiares})
